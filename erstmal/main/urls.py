d5303864089ee84295635681acde77d41924c817
razdor13 <oneamaday@gmail.com>
2025-05-11 23:19:14 +0200

    work with templates and other

--- erstmal/main/urls.py
+++ erstmal/main/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index), path("about", views.about)]
+urlpatterns = [
+    path("", views.index, name="home"),
+    path("about", views.about, name="about"),
+]
